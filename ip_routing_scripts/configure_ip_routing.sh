90491ad290c075f1fde6b93c63fcdafe26f03c3f
psherman-uva <ukw4tc@virginia.edu>
2024-08-09 09:10:40 -0400

    Fix bug in IP route scripte

--- ip_routing_scripts/configure_ip_routing.sh
+++ ip_routing_scripts/configure_ip_routing.sh
@@ -8,8 +8,8 @@ python3 gen_ip_route_files.py
 # Move generated files
 echo "Moving generating file"
 
+sudo cp ./ip_route.service /etc/systemd/system/
 sudo mv ./ip_route.sh /usr/local/sbin/
-sudo mv ./ip_route.service /etc/systemd/system/
 sudo mv ./fastdds_discovery_super_client.xml /etc/turtlebot4_discovery/
 sudo mv ./setup.bash /etc/turtlebot4_discovery/
 
@@ -20,4 +20,6 @@ sudo systemctl enable ip_route.service
 sudo systemctl restart ip_route.service
 
 # End of script
-echo "Source your ~/.bashrc file to apply changes"
+echo "Sourcing ~/.bashrc file to apply changes"
+
+source ~/.bashrc
